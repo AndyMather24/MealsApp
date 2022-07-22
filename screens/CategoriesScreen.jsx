a933c53e345a2d59ecde89b32efd4584d388b7be
andy mather <anyd-mather@live.co.uk>
2022-07-22 14:15:12 +0100

    categories screen

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,41 @@
+import { FlatList } from "react-native";
+import CategoriesGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from '../data/dummy_data'
+
+
+
+
+function CategoriesScreen({ navigation, })
+{
+
+
+
+    function renderCategoryItem(itemData)
+    {
+        function pressHandler()
+        {
+            navigation.navigate("MealsOverview", {
+                categoryId: itemData.item.id
+            })
+
+
+        }
+
+        return (
+            <CategoriesGridTile onPress={pressHandler} title={itemData.item.title} color={itemData.item.color} />
+        )
+    }
+
+
+    return (
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={item => item.id}
+            renderItem={renderCategoryItem}
+            numColumns={2}
+
+        />
+    )
+}
+
+export default CategoriesScreen;
